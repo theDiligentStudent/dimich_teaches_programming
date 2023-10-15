890c1fceb354706884c8dd796069714b40bbf266
theDiligentStudent <ivanrenkin14@gmail.com>
2023-10-15 22:27:43 +0300

    Dimich Teaches Programming

---
+++ lesson_4/lesson_4.js
@@ -0,0 +1,26 @@
+//    Data types and Variables
+
+let sportsman = {
+  height: {
+    number: 187,
+    heightUnits: 'centimeters',
+  },
+  isThisPersonComing: true,
+  name: 'Albert',
+  isThisPersonAWoman: false,
+};
+
+let runner = {
+  name: 'Victor',
+  weightMeasurement: 'kilogram',
+  lengthMeasurement: 'centimeter',
+  weight: 87,
+  length: 180,
+  hadLongHair: false,
+};
+
+let autocar = {
+  isAnSuv: false,
+  haveWinterTires: true,
+  
+}
