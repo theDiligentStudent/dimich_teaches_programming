2336e74239ca92b7b58e03e348ceec11316b1900
theDiligentStudent <ivanrenkin14@gmail.com>
2023-10-31 21:37:10 +0200

    Вспоминал Калькулятор с помощью  chat gpt.  Оказывается (я и забыл Совсем) чтобы Ф. выполнилась --- П.-е надо Обхявлять -- ВНУТРИ НЕЁ. Сейчас всё заработало -- даже -- С чатом, правда, Но Вспомнил -- как складывать Числа -- чтобы они НЕ склеивались Как Строки..... Теперь ещё нужно Разобраться/Вспомнить currenttarget и потом Ф.-ю с if/else

--- lesson_6/myself_6.js
+++ lesson_6/myself_6.js
@@ -2,16 +2,17 @@
 // Вспоминаю КАК (??) делается Калькулятор
 // потому что за эти месяца 3-4, что я НЕ (!!) повторял.... --- СОВСЕМ ЗАБЫЛ
 
-let input1 = 'number_1';
-let valueInput1 = document.getElementById(input1);
-let input2 = 'number_2';
-let valueInput2 = document.getElementById(input2);
+// let input1 = 'number_1';
+// let valueInput1 = document.getElementById(input1);
+let valueInput1 = document.getElementById('number_1');
+// let input2 = 'number_2';
+// let valueInput1 = document.getElementById(input2);
+let valueInput2 = document.getElementById('number_2');
 
 let buttonPlus = 'button_plus';
 let tapButtonPlus = document.getElementById(buttonPlus);
 
-
-// Вот Здесь (ниже)..... --- я НЕ могу вспомнить -- 
+// Вот Здесь (ниже)..... --- я НЕ могу вспомнить --
 //    КАК получить ОБЩУЮ (УНИВЕРСАЛЬНУЮ) Ф.-ю для доступа ко ВСЕМ элементам по ID
 //    ---
 //    Пробую -- но пока вспомнить .... НЕ получается.....
@@ -22,10 +23,9 @@ let tapButtonPlus = document.getElementById(buttonPlus);
 // // accessToElement(buttonPlus, 'button_plus');
 // access(buttonPlus, 'button_plus');
 
-
 // let clickOnButtonPlus = tapButtonPlus.addEventListener('click', clickPlus);
 
-function clickPlus () {
+function clickPlus() {
   alert('Hello all');
 }
 
@@ -36,12 +36,24 @@ function clickPlus () {
 //   // eventObject.currentTarget
 // }
 
-let getInput1 = valueInput1.value;
-let getInput2 = valueInput2.value;
+// let getInput1 = valueInput1.value;
+// let getInput2 = valueInput2.value;
 function minusNumber() {
-  let result = getInput1 - getInput2;
+  // П.-е ОБЯЗАТЕЛЬНО (!!!!) должны быть ВНУТРИ Ф.-и!!!!!
+  //  (а НЕ так -- как у меня записано выше)
+  let getInput1 = valueInput1.value;
+  let getInput2 = valueInput2.value;
+  // СЛОЖЕНИЕ ЧИСЕЛ
+  //  Таким Вот Образом числа  JS -- НЕ сложатся!!!!, а СКЛЕЯТСЯ.....
+  // let result = getInput1 + getInput2;
+  //  ДЛЯ ОПЕРАЦИИ СЛОЖЕНИЯ ---- К. НУЖНО ПИСАТЬ ВОТ ТАК !!!!!  ----
+  let result = +getInput1 + +getInput2;
   // return result;
   window.alert(result);
 }
-let clickOnButtonPlus1 = tapButtonPlus.addEventListener('click', minusNumber);
-clickOnButtonPlus1();
+
+// так (через let) -- НЕ нужно писать
+// let clickOnButtonPlus1 = tapButtonPlus.addEventListener('click', minusNumber);
+// ТОЛЬКО ТАК
+clickOnButtonPlus1 = tapButtonPlus.addEventListener('click', minusNumber);
+// clickOnButtonPlus1();   Эта строчка -- НЕ -- нужна
