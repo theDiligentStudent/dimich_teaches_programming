2b696119a307f53554c36e0e2d71d6d3759bcf1b
theDiligentStudent <ivanrenkin14@gmail.com>
2023-11-18 11:19:12 +0200

    18/11/23  -- Вчера сделал Рефакторинго Пор.-ы Дмыча.... - Теперь она работает практически так - как говорил (задумал) Димыч в Начале Ролика, за исключением того - что в конце НеТ Сообщения о количестве Правильных Ответов в Процентах.......   Это я сейчас *18/11/23* постараюст Исправить/Долелать.... - chatGPT уже подсказал Решение. -- Когда буду пробовать или Сделаю -- то Закомитчу Изменения

--- lesson_6/myself_js/myself_7.js
+++ lesson_6/myself_js/myself_7.js
@@ -10,34 +10,69 @@
 //     Вчера (15/11/23) -- решил сделать Немного ПО-ДРУГОМУ (не так, как написал во вчерашнем комментарии) - а вспомнил, что смотрел Маленький Кусочек (начало самое) 6-го ролика --- и в нём видел - Как - Димыч выводит работу Про.-ы на экран Бр.-а....
 //     -- я задумался о том, как он это Реализует и (Сам) вспомнил про method prompt -- стал Пробовать ---- и у меня - хоть и не сразу - но кажется (почти всё) начало и стало Получаться.....
 //     Сейчас буду Пробовать реализовать Про.-у. Как можно полнее.  Проценты сделать конечно я НЕ смогу но Всё Остальное -- Сегодя сделать Попытаюсь......
+// 17/11/23
+// Продолжаю возиться с Про.-й Димыча
+// Сегодя надо добавть к уже Имеющимся Двум Словам на изучение - ещё Три.
+// И сделать Ф.-ю с Сообщением Результат Тренировки.
+// + Почищу К. от вчерашних Комментариев с Попытками
 
+let counter = 0;
 function showAndGiveWord(word1, word2) {
-  // let counter = 0;
+  // let counter = 0;   // Эту строку ВЫНЕС -- НАРУЖУ Ф.-И -- Иначе НЕ производится Подсчёт, а каждый раз (при Выполнении Ф.-и) - counter - берётся равным 0-лю!!!!
+  function doCounter() {
+    counter++;
+    alert(counter);
+  }
   let mainWord = prompt(word1);
   if (mainWord === word2) {
-    // counter++;
     alert('Well done!');
-    // alert(counter);
+    doCounter();
   } else {
     alert('Think about it!');
   }
 }
 
-let counter = 0;
-function doCounter() {
-  counter++;
-  alert(counter);
-}
+// let counter = 0;
+// function doCounter() {
+//   counter++;
+//   alert(counter);
+// }
 
 function studyWord() {
   showAndGiveWord('Я', 'I');
-  // counter++;
-  // alert(counter);
-  doCounter();
+  // doCounter();
   showAndGiveWord('Живу', 'Live');
-  doCounter();
-  // counter++;
-  // alert(counter);
+  // doCounter();
+  showAndGiveWord('My', 'Мой');
+  showAndGiveWord('Name', 'Имя');
+  showAndGiveWord('Твой', 'Your');
 }
 
 studyWord();
+
+function result() {
+  alert(counter);
+  if (counter === 3) {
+    alert('Not bad!');
+  }
+  if (counter === 4) {
+    alert('Fine!');
+  }
+  if (counter === 5) {
+    alert('Generally Excellent!!');
+  }
+  if (counter === 1) {
+    alert('Try harder (');
+  }
+  if (counter === 2) {
+    alert('Try harder (');
+  }
+  // else {
+  //   alert('Try harder (');
+  // }
+}
+result();
+
+counter = 0;
+studyWord();
+result();
