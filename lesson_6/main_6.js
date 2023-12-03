99f9ef010cd08b1d56bf3eaf01d4d4adb7a22da2
theDiligentStudent <ivanrenkin14@gmail.com>
2023-12-03 23:35:38 +0200

    3/12/23 ---- Работал с 6-м видеоуроком Димыча. Дошёл до 27-й минуты. Изменял files: index_6.html and main_6.js

--- lesson_6/main_6.js
+++ lesson_6/main_6.js
@@ -21,97 +21,12 @@
 // Повспоминал как работает method prompt --- а то забыл уже...... - Димыч в видео уже перешёл к нему - поэтому я попробовал Вспомнить Сам - увидел - что Забыл..... -- и стал вспоминать.
 // 2/12/23
 // Сперва напишу по памяти всю основную Про.-у, которую написал Димыч в своём 6-м видео к 19-й минуте. (Это Повторение заняло у меня 40 минут) -- Потом продолжу смотреть видео с Димычем.
+// 3/12/23
+// Сейчас буду смотреть видео (6-е) Димыча дальше, с 22-й минуты.
+// Но - сперва - поудаляю много комментариев, работы (более ранней) над К.-м Про.-ы.
 //  ---------------------
 //   ------------------------
 //   -------------------------------
-
-//
-//
-//
-//====
-//ТРЕТИЙ Вариант Про.-ы
-//
-// (Подготовка/инициализация Д.-х)
-// let rusWord0 = 'безопасность'; // 0
-// let engWord0 = 'safety';
-
-// let rusWord1 = 'испуг'; // 1
-// let engWord1 = 'fright';
-
-// let rusWord2 = 'сострадание, сочувствие, жалость'; // 2
-// let engWord2 = 'compassion';
-
-// // Ввожу П.-ю С ПОДСКАЗКОЙ - во внутрь prompt
-// let clue = 'введи Английское слово';
-
-// // П. с Конечным (поощрительным) Текстом
-// let finalMessage = 'Well done, but keep trying.';
-
-// // =============
-// // Непосредственно Сама Про.
-// // заменяю выводы в alert-ах Словами, на выводы П.-ми
-
-// // Теперь МЕНЯЮ ВЫВОДЫ alert на prompt
-// // Кстати, --- очень интересный -- вариант Про.-ы теперь получается...
-// prompt(rusWord0, clue); // 0  //Использую П.-ю clue -- вместо строки с текстом 'введи Английское слово'
-// alert(engWord0);
-
-// prompt(rusWord1, clue); // 1   //Использую П.-ю clue -- вместо строки с текстом 'введи Английское слово'
-// alert(engWord1);
-
-// prompt(rusWord2, clue); // 2   //Использую П.-ю clue -- вместо строки с текстом 'введи Английское слово'
-// alert(engWord2);
-
-// alert(finalMessage);     // вывод на экран Финального сообщения
-//
-//
-//
-// (Подготовка/инициализация Д.-х)
-// let rusWord0 = 'безопасность'; // 0
-// let engWord0 = 'safety';
-
-// let rusWord1 = 'испуг'; // 1
-// let engWord1 = 'fright';
-
-// let rusWord2 = 'сострадание, сочувствие, жалость'; // 2
-// let engWord2 = 'compassion';
-
-// let showWord = prompt(word, clue);  // Так -- НЕ работает
-
-// Ввожу П.-ю С ПОДСКАЗКОЙ - во внутрь prompt
-// let clue = 'введи Английское слово';
-
-// П. с Конечным (поощрительным) Текстом
-// let finalMessage = 'Well done, but keep trying.';
-
-// =============
-// Непосредственно Сама Про.
-// заменяю выводы в alert-ах Словами, на выводы П.-ми
-
-// Теперь МЕНЯЮ ВЫВОДЫ alert на prompt
-// Кстати, --- очень интересный -- вариант Про.-ы теперь получается...
-// showWord(rusWord0, clue); // 0  // Так -- НЕ работает
-// function learnWords(rusWord, engWord) {
-//     // let word;
-//     // let word1;
-//     let showWord = prompt(rusWord, clue);
-//     if(showWord === engWord) {
-//         alert('Hi!');
-//     }
-//     else {
-//         alert('By');
-//     }
-// }
-// learnWords(rusWord0, engWord0);
-// learnWords(rusWord1, engWord1);
-// learnWords(rusWord2, engWord2);
-// learnWords('safety');
-
-// prompt(rusWord1, clue); // 1   // Так -- НЕ работает
-
-// prompt(rusWord2, clue); // 2   // Так -- НЕ работает
-
-// alert(finalMessage); // вывод на экран Финального сообщения
 
 // ==================
 
@@ -129,28 +44,78 @@ let engWord2 = 'are you offended?';
 
 let promptOutput = 'enter a word in English';
 
+let counter = 0; // enter the counter variable
+
 let finalMessage = 'Well done, but try harder!'; // final message
 //
 //
-// working code block  (execution block)
-// alert(rusWord0); // 1
-// function wordCheck() {
-let word0 = prompt(rusWord0, promptOutput); // 1
-alert(word0 === engWord0);
-// if(word0 === engWord0) {
-//     alert('Hi!');
-// } else {
-//     alert('By...');
+// // working code block 1 (execution block)
+
+// let wordWithAnswer0 = prompt(rusWord0, promptOutput); // 1
+// alert(wordWithAnswer0 === engWord0);
+
+// // alert(rusWord1); // 2
+// let wordWithAnswer1 = prompt(rusWord1, promptOutput); // 2
+// alert(wordWithAnswer1 === engWord1);
+
+// // alert(rusWord2); // 3
+// let wordWithAnswer2 = prompt(rusWord2, promptOutput); // 3
+// alert(wordWithAnswer2 === engWord2);
+
+// alert(finalMessage); // final message/
+//
+//
+// // working code block 2 (execution block)
+
+// let wordWithAnswer0 = prompt(rusWord0, promptOutput); // 1
+// alert(wordWithAnswer0 === engWord0);
+// if (wordWithAnswer0 === engWord0) {
+//   counter++;
+//   alert(counter);
 // }
+
+// // alert(rusWord1); // 2
+// let wordWithAnswer1 = prompt(rusWord1, promptOutput); // 2
+// alert(wordWithAnswer1 === engWord1);
+// if (wordWithAnswer1 === engWord1) {
+//   counter++;
+//   alert(counter);
+// }
+
+// // alert(rusWord2); // 3
+// let wordWithAnswer2 = prompt(rusWord2, promptOutput); // 3
+// alert(wordWithAnswer2 === engWord2);
+// if (wordWithAnswer2 === engWord2) {
+//   counter++;
+//   alert(counter);
+// }
+
+// alert(finalMessage); // final message/
+//
+//
+// working code block 3 (execution block)
+
+let wordWithAnswer0 = prompt(rusWord0, promptOutput); // 1
+alert(wordWithAnswer0 === engWord0);
+if (wordWithAnswer0 === engWord0) {
+  counter++;
+  alert(counter);
+}
 
 // alert(rusWord1); // 2
-let word1 = prompt(rusWord1, promptOutput); // 2
-alert(word1 === engWord1);
+let wordWithAnswer1 = prompt(rusWord1, promptOutput); // 2
+alert(wordWithAnswer1 === engWord1);
+if (wordWithAnswer1 === engWord1) {
+  counter++;
+  alert(counter);
+}
 
 // alert(rusWord2); // 3
-let word2 = prompt(rusWord2, promptOutput); // 3
-alert(word2 === engWord2);
+let wordWithAnswer2 = prompt(rusWord2, promptOutput); // 3
+alert(wordWithAnswer2 === engWord2);
+if (wordWithAnswer2 === engWord2) {
+  counter++;
+  alert(counter);
+}
 
 alert(finalMessage); // final message/
-// }
-// wordCheck();
