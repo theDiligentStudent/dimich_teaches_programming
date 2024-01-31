9cf13526d91110af70fe23172bc54cf6c4f672eb
theDiligentStudent <ivanrenkin14@gmail.com>
2024-01-31 15:45:47 +0200

    28/01/24 ---- Продолжаю смотреть видеоурок Димыча №8 -- о Ф.-х и копипастить за ним -- пытаюсь разобраться. --- alert, prompt, document.

---
+++ lesson_8/with_dimich/js/first.js
@@ -0,0 +1,26 @@
+// 28/01/24
+// Продолжаю смотреть видеоурок Димыча №8 -- о Ф.-х
+// и копипастить за ним -- пытаюсь разобраться
+// alert, prompt, document.
+
+// // //
+// // //
+// // //
+
+// document.write('hi');   // ПравильнаЯ Запись
+
+// document.write(hi);  // НЕПРАВИЛЬНАЯ запись
+
+// /// //
+
+const massage = 'Hello!!';
+
+// let resultOfFunction = window.alert(massage);
+
+let resultOfFunction = document.write(massage);
+document.write(massage);
+
+// let resultOfFunction = window.console.log(massage);
+// resultOfFunction;
+// resultOfFunction();
+// resultOfFunction('HI');
