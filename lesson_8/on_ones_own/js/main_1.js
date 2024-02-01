9b43679809683c514ac4de8512175bc7f4cae4e3
theDiligentStudent <ivanrenkin14@gmail.com>
2024-02-01 21:04:08 +0200

    31/01/24 ---- Продолжаю смотреть видеоурок Димыча №8 -- о Ф.-х. с 12й минуты, Посмотрел/Поделал по 20-ю минуту включительно. плюс --- создал ещё по одному html,js-файлу в папке on_ones_onwn -- чтобы начать - заново - повторять/писать про.-у из ур. 6 - но уже используя об./ф.-ю documetn.write

---
+++ lesson_8/on_ones_own/js/main_1.js
@@ -0,0 +1,19 @@
+// 31/01/24
+// Смотрю 8ой Ур Димыча ---- Повторяю Сам
+
+//
+
+// I. B.
+
+const englishWord0 = ''; // 0
+const russianWord0 = '';
+
+const englishWord1 = ''; // 1
+const russianWord1 = '';
+
+const englishWord2 = ''; // 2
+const russianWord2 = '';
+
+//
+
+// C. B.
--- lesson_8/with_dimich/js/first.js
+++ lesson_8/with_dimich/js/first.js
@@ -3,6 +3,11 @@
 // и копипастить за ним -- пытаюсь разобраться
 // alert, prompt, document.
 
+// 31/01/24
+// Продолжаю смотреть видеоурок Димыча №8 -- о Ф.-х
+// с 12й минуты
+// Посмотрел/Поделал по 20-ю минуту включительно
+
 // // //
 // // //
 // // //
@@ -13,14 +18,39 @@
 
 // /// //
 
-const massage = 'Hello!!';
+const message = 'Hello!!';
 
 // let resultOfFunction = window.alert(massage);
 
-let resultOfFunction = document.write(massage);
-document.write(massage);
+// let resultOfFunction = document.write(message);
+// document.write(message);
+// alert(message);
 
 // let resultOfFunction = window.console.log(massage);
 // resultOfFunction;
 // resultOfFunction();
 // resultOfFunction('HI');
+
+// // //
+// // //
+// // //
+
+// 31/01/24
+const message1 = 'Enter your message';
+const userInput = prompt(message1);
+// const br = document.write('<br>'); // так НЕ работает
+const userResponse = document.write(userInput);
+
+// document.write(' ' + userInput);
+// document.write('<br>');
+// document.write('</br>');  // можно и так ставить --- Димыч ставит Так
+
+// br;   // так НЕ работает
+// br(); // так НЕ работает
+document.write(' Your input is: ' + userInput);
+
+// а РАБОТАЕТ - Вот Так !!  -- как предлагает Димыч!
+document.write(' Your input is: ' + userInput + '</br>');
+document.write(' Your input is: ' + userInput + '</br>');
+document.write(' Your input is: ' + userInput + '</br>');
+document.write(' Your input is: ' + userInput + '</br>');
