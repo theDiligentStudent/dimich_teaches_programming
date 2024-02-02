e1cbd2df4353e5a44dec830f62d8bb746e17f651
theDiligentStudent <ivanrenkin14@gmail.com>
2024-02-02 20:15:18 +0200

    1/02/24 ---- Работаю в Папке on_ones_own. 1/02/24   Пишу Заново (в смысле Самостоятельно)Про.-у Димыча из 6го Ур.-а, но буду делать уже с Заменой вызова window.alert на document.write --так - как показывает Димыч в 8м Ур.-е Уже столкнулся с Первой Трудностью/НЕпоняткой. - При вызове document.write..... - - может быть разберусь с ней - когда (позже) досмотрю В. Димыча до конца.

--- lesson_8/on_ones_own/js/main_1.js
+++ lesson_8/on_ones_own/js/main_1.js
@@ -1,19 +1,60 @@
 // 31/01/24
 // Смотрю 8ой Ур Димыча ---- Повторяю Сам
 
+// 1/02/24
+// Пишу Занову
+// (в смысле Самостоятельно)
+// Про.-у Димыча из 6го Ур.-а, но буду делать уже
+// с Заменой вызова window.alert на document.write --
+// так - как показывает Димыч в 8м Ур.-е
+// Уже столкнулся с Первой Трудностью/НЕпоняткой. - При вызове document.write..... - - может быть разберусь с ней - когда (позже) досмотрю В. Димыча до конца
+
 //
 
 // I. B.
 
-const englishWord0 = ''; // 0
-const russianWord0 = '';
+const russianWord0 = 'Выше';
+const englishWord0 = 'Above'; // 0
 
-const englishWord1 = ''; // 1
-const russianWord1 = '';
+const russianWord1 = 'Прямо над (чем-то)';
+const englishWord1 = 'Right above'; // 1
 
-const englishWord2 = ''; // 2
-const russianWord2 = '';
+const russianWord2 = 'Произвольный';
+const englishWord2 = 'Arbitrary'; // 2
+
+const numberOfWordsStudied = 3;
+let numberOfCorrectAnswers = 0;
+
+const promptHelpText = 'Enter a word or group of words in English';
+
+// let showWordAcceptAnswer0;
+// let showWordAcceptAnswer1;
+// let showWordAcceptAnswer2;
+let showWordAcceptAnswer;
+// const tagBr = '</br>';
+// const tagBr1 = '</br> </br>';
+
+let determinePercentageCorrectAnswers;
+const numberForStoringCorrectAnswers = 50;
 
 //
 
 // C. B.
+
+function toShowPrompt(russianWord, englishWord) {
+  showWordAcceptAnswer = prompt(russianWord, promptHelpText);
+  document.write(englishWord === showWordAcceptAnswer);
+  // document.write(englishWord === showWordAcceptAnswer, '</br>');
+  // document.write(englishWord === showWordAcceptAnswer, '</br> </br>');
+  // document.write(englishWord === showWordAcceptAnswer, tagBr);
+  if (englishWord === showWordAcceptAnswer) {
+    numberOfCorrectAnswers = numberOfCorrectAnswers + 1;
+  } else {
+    document.write(englishWord);
+    // document.write(englishWord, tagBr);
+  }
+}
+
+toShowPrompt(russianWord0, englishWord0);
+toShowPrompt(russianWord1, englishWord1);
+toShowPrompt(russianWord2, englishWord2);
