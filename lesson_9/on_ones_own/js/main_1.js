0addda42eb0b032444464c6e4eccb205109b308e
theDiligentStudent <ivanrenkin14@gmail.com>
2024-02-11 14:46:07 +0200

    10/02/24 ---- Урок Димыча (даже не знаю Как его назвать) 5а (что ли) --- Про П.-е, визуализацию Д.-х и Практику. Он появился на канале не с того ни с сего

--- lesson_9/on_ones_own/js/main_1.js
+++ lesson_9/on_ones_own/js/main_1.js
@@ -2,12 +2,34 @@
 // Сейчас Начинаю Смотреть (с Димычем) -- А кое-что --- буду пробовать делать САМ
 // 9 Ур. - Условная Логика - Ветвление Про.-ы
 
+// 5/02/24
+// Сейчас  -- на основании того, что увидел в Ур.-е Димыча
+// (посмотрел где-то 3 минуты)
+// Попробую сделать Про.-ку по ---- Покупке 5 Смартфонов ----
+// ---- с помощью 5ти Кнопок и 5-ти addEventListener-ов к ним....
+
 // /// // ///
 
 // I. B.
 
-const promptHint = 'Deposit $40';
-const amountForComparison = 40;
+const btnIPhone = accessToPhoneIds('phone_iphone');
+const btnSamsung = accessToPhoneIds('phone_samsung');
+const btnXiaomi = accessToPhoneIds('phone_xiaomi');
+const btnHuawei = accessToPhoneIds('phone_huawei');
+const btnZte = accessToPhoneIds('phone_zte');
+
+let smartphonePrice;
+const promptHint = 'Deposit ' + smartphonePrice;
+
+const priceIPhone = 200;
+const priceSamsung = 170;
+const priceXiaomi = 140;
+const priceHuawei = 120;
+const priceZte = 100;
+
+// const amountForComparison = 40;
+let amountForComparison;
+
 const tagBr = '</br> </br>';
 
 // //
@@ -22,24 +44,14 @@ const tagBr = '</br> </br>';
 
 // Сейчас Попробую Немного Доработать этот Функционал - из видео Димыча
 
-// function productPurchase() {
-//     document.write('START', tagBr);
-//   if (true) {
-//     // if(false) {
-//     document.write('Product Purchased!', tagBr);
-//   } else {
-//     document.write('There are not enough funds in your account', tagBr);
-//   }
-//   document.write('END');
-//   document.write(tagBr, 'END'); // Димыча - Более Правильная и Логичная запись
-// }
+function accessToPhoneIds(id) {
+  let phoneId = document.getElementById(id);
+  return phoneId;
+}
 
-// function startProgramme() {
-//   // document.write('START', tagBr);
-//   alert('START', tagBr);
-// }                               // ЛИШНЯЯ Ф.
+// accessToPhoneIds();   // так делать НЕ нужно   --- Всё Работает и БЕЗ вызова этой Ф.-и
 
-function productPurchase() {
+function productPurchase(currentProductPurchase) {
   alert('START', tagBr);
   const requestAmountAndAccept = prompt(promptHint);
   if (requestAmountAndAccept >= amountForComparison) {
@@ -50,11 +62,42 @@ function productPurchase() {
   }
   // document.write('END');
   document.write(tagBr, 'END'); // Димыча - Более Правильная и Логичная запись
+}  
+
+function currentTarget(evenObject) {
+  // let currentValue = currentTarget.evenObject(id);
+  let currentValue = evenObject.currentTarget(id);
+  return currentValue;
+}
+
+function currentProductPurchase(currentTarget) {
+  if (currentTarget === 'IPhone') {
+    amountForComparison = 200;
+  }
+  if (currentTarget === 'Samsung Galaxy') {
+    amountForComparison = 170;
+  }
+  if (currentTarget === 'Xiaomi') {
+    amountForComparison = 140;
+  }
+  if (currentTarget === 'Huawei') {
+    amountForComparison = 120;
+  }
+  if (currentTarget === 'ZTE') {
+    amountForComparison = 100;
+  }
 }
 
 //
 
 // Function call block
 
-// startProgramme();  // ЛИШНЯЯ Ф.
-productPurchase();
+// productPurchase();
+
+// const btnIPhone = accessToPhoneIds('phone_iphone');
+// btnIPhone.addEventListener('click', productPurchase(priceIPhone));
+btnIPhone.addEventListener('click', productPurchase);
+btnSamsung.addEventListener('click', productPurchase);
+btnXiaomi.addEventListener('click', productPurchase);
+btnHuawei.addEventListener('click', productPurchase);
+btnZte.addEventListener('click', productPurchase);
