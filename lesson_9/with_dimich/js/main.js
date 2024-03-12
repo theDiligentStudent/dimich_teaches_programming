2689b478edcf90a2787cf9b3fd2e8af057aa31ee
theDiligentStudent <ivanrenkin14@gmail.com>
2024-03-13 00:24:24 +0200

    12/03/24 ---- то, что сделал для этого  коммита - описал в изменённых файлах (самостоятельно ---- работаю с 9-м ур.-м д.в.-а по ветвлению про.-ы)

--- lesson_9/with_dimich/js/main.js
+++ lesson_9/with_dimich/js/main.js
@@ -20,6 +20,9 @@
 // -----
 // Осталось только Поудалять Практически Все Комментарии
 // (за Исключением Некоторых) -- и сразу опять Закоммитить
+// ---------------
+// Поудалял как можно больше (лишних) комментариев
+// Теперь буду Коммитить Изменения
 
 // /// // ///
 // /// // ///
@@ -27,22 +30,13 @@
 
 // I. B.
 
-// const productTheCost = 120;
-// let cardMoneyAmount1 = 100;
-// let cardMoneyAmount2 = 120;
-// let cardMoneyAmount3 = 200;
-
 const operationStart = 'START';
+
 let productPrice;
+
 const forPromptText = 'How much does this product cost?';
 
-// let balance1 = 100;
-// let balance2 = 120;
-// let balance3 = 200;
 let cardsBalance = 200;
-// const productPrice = window.prompt('How much does this product cost?');
-// let productPrice = window.prompt('How much does this product cost?');
-// let cardsFundsBalance;
 
 const productPurchased = 'The product is purchased';
 const lackOfFunds = 'There are insufficient funds on the card';
@@ -52,130 +46,21 @@ const operationEnd = 'END';
 
 // C. B.
 
-// document.write('START', tagBr, 'Product Purchased', tagBr, 'END');
-
-//
-
 // Function declaration block
 
 // 10/30/24
 // Ещё раз играемся и Изменяем К.
-function goodsPurchase() {
-  // document.write('<br>', 'START', '<br>');
-  alert('START');
-  // if (balance2 >= productPrice) {
-  if (balance3 < productPrice) {
-    // document.write('There are insufficient funds on the card');
-    alert('There are insufficient funds on the card');
-  } else {
-    // document.write('The product is purchased');
-    alert('The product is purchased');
-  }
-  balance3 = balance3 - productPrice;
-  alert(balance3);
-  // document.write('<br>', 'END');
-  alert('END');
-}
-
-// function goodsPurchase1(balance) {
-// function goodsPurchase1() {
-//   alert(cardsBalance);
-//   alert('START');
-//   const productPrice = window.prompt('How much does this product cost?');
-//   if (cardsBalance < productPrice) {
-//     alert('There are insufficient funds on the card');
-//   }
-//   // else if (cardsBalance === 0) {
-//   //   alert('The operation has been cancelled. The balance on the card is 0.'); }
-//   else {
-//     alert('The product is purchased');
-//   }
-
-//   if (cardsBalance >= productPrice) {
-//     cardsBalance = cardsBalance - productPrice;
-//   }
-
-//   if (cardsBalance > 0) {
-//     alert(cardsBalance);
-//   } else {
-//     alert('The card balance is 0.');
-//   }
-//   alert('END');
-// }
-
-//////
-
-// Играюсь с К.-м, - как учил Д. В. --
-// Меняю местами Опн.-ы - Изменяю Оп.-ы
-// Всё Успешно Получилось
-// function goodsPurchase1() {
-//   alert(cardsBalance);
-//   alert('START');
-//   const productPrice = window.prompt('How much does this product cost?');
-//   if (productPrice > cardsBalance) {
-//     alert('There are insufficient funds on the card');
-//   }
-//   // else if (cardsBalance === 0) {
-//   //   alert('The operation has been cancelled. The balance on the card is 0.'); }
-//   else {
-//     alert('The product is purchased');
-//   }
-
-//   if (productPrice <= cardsBalance) {
-//     cardsBalance = cardsBalance - productPrice;
-//   }
-
-//   if (cardsBalance > 0) {
-//     alert(cardsBalance);
-//   } else {
-//     alert('The card balance is 0.');
-//   }
-// }
-
-////////////
 
 // Закомментирую эту Ф.-ю чтобы Зарефакторить мою/Д. В.-а Про.-у
 // ---
 // Вынесу все Стр.-ые Д. в I. B. и позаношу их в П.-е
-
-// function goodsPurchase1() {
-//   alert(cardsBalance);
-//   alert('START');
-//   const productPrice = window.prompt('How much does this product cost?');
-//   if (productPrice <= cardsBalance) {
-//     alert('The product is purchased');
-//   }
-//   // else if (cardsBalance === 0) {
-//   //   alert('The operation has been cancelled. The balance on the card is 0.'); }
-
-//   // else if (productPrice === '') { alert('There are insufficient funds on the card');}
-//   // К. в Этой Строке почему-то НЕ получилось реализовать.....
-//   ///////
-//   else {
-//     alert('There are insufficient funds on the card');
-//   }
-
-//   if (productPrice <= cardsBalance) {
-//     cardsBalance = cardsBalance - productPrice;
-//   }
-
-//   if (cardsBalance > 0) {
-//     alert(cardsBalance);
-//   } else {
-//     alert('The card balance is 0.');
-//   }
-
-//   // alert(cardsBalance);
-
-//   alert('END');
-// }
 
 ///////////
 
 // Рефакторю мой К./мою Ф.-ю
 // Заношу все Стр.-ые Д. в П.-е
 
-function goodsPurchase1() {
+function goodsPurchase() {
   alert(cardsBalance);
   alert(operationStart);
   // const productPrice = window.prompt(forPromptText);
@@ -183,9 +68,6 @@ function goodsPurchase1() {
   if (productPrice <= cardsBalance) {
     alert(productPurchased);
   }
-  // else if (cardsBalance === 0) {
-  //   alert('The operation has been cancelled. The balance on the card is 0.'); }
-
   // else if (productPrice === '') { alert('There are insufficient funds on the card');}
   // К. в Этой Строке почему-то НЕ получилось реализовать.....
   ///////
@@ -203,18 +85,14 @@ function goodsPurchase1() {
     alert(whenBalanceOnCardIs0);
   }
 
-  // alert(cardsBalance);
-
   alert(operationEnd);
 }
 
 /////////////////////////
 
 // Function call block
-goodsPurchase1();
-goodsPurchase1();
-goodsPurchase1();
+goodsPurchase();
+goodsPurchase();
+goodsPurchase();
 
-// goodsPurchase();
 
-// window.document.write('Hello!');
