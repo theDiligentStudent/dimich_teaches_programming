087adb41670a59c9afdacb4d0509c1df22399367
theDiligentStudent <ivanrenkin14@gmail.com>
2024-04-04 21:28:07 +0300

    4/04/24  ---- Смотрел/делал 11й Ур. димыча - про об.-ы до 14й минуты (до вложенности/влаживаемых об.-в)

--- second_dozen/lesson_11/with_dimich/js/main_1.js
+++ second_dozen/lesson_11/with_dimich/js/main_1.js
@@ -1,8 +1,12 @@
-// 4/02/24
+// 2/04/24
 // Начинаю 11-й Ур. Димыча (у него в п/л.-е он уже 15-й)
 
-// 5/02/24
+// 3/04/24
 // Продолжаю смотреть Ур. Димыча, но, можно сказать - с самого начала
+// посмотрел/поделал до 12й минуты
+
+//   4/04/24
+//  Продолжаю смотреть Ур. Димыча, c 12-й минуты
 
 // alert('Hello');  // Проверка подключения JS-файла к html
 
@@ -14,7 +18,7 @@ myConsole = {
   isMarried: true,
 };
 
-window.alert(myConsole.isMarried); // Показать З. Сво-ва Об.-а
+// window.alert(myConsole.isMarried); // Показать З. Сво-ва Об.-а   по-другому -- мы можем Обратиться к Сво-ву Об.-а
 
 myConsole = {
   name: 'Alex',
@@ -22,4 +26,26 @@ myConsole = {
   age: 40,
 };
 myConsole.age = 41;
-window.alert(myConsole.age); // Показать З. Сво-ва Об.-а
+// window.alert(myConsole.age); // Показать З. Сво-ва Об.-а
+
+// 3/04/24
+
+// Object - Real Object
+let laptop1 = {
+  name: 'ZTE',
+  manufacturer: 'Zenbook',
+  price: 1000,
+};
+let laptop2 = {
+  name: 'MacBook',
+  manufacturer: 'APPLE',
+  price: 2000,
+};
+
+// Object - Process
+const fundsTransfer = {
+  from: 'Alexander1',
+  // to: 'Alexander2',
+  to: 'bookstore Ranok',
+  transferAmount: 450,
+};
